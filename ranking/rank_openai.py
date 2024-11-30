e46e1fc72c9ccbabd34c208234266b073dd9cf38
Rohit Vernekar <rohitvernekar76@gmail.com>
2024-11-30 16:09:51 -0500

    Adding code to rank passages using OpenAI Embeddings

---
+++ ranking/rank_openai.py
@@ -0,0 +1,13 @@
+from langchain_community.embeddings import OpenAIEmbeddings
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+
+open_ai_key = ""
+def rank_passages_openai(query, passage_text):
+    embedding_model = OpenAIEmbeddings(model="text-embedding-3-small", openai_api_key=open_ai_key)
+    query_embedding = np.array(embedding_model.embed_query(query)).reshape(1, -1)
+    passage_embeddings = embedding_model.embed_documents(passage_text)
+
+    similarity_scores = cosine_similarity(query_embedding, passage_embeddings).flatten()
+    ranked_indices = np.argsort(similarity_scores)[::-1]
+    return ranked_indices
